5b42f5786a067d5f3c56a44fdf6b7619ea283737
El Psy Congroo <mcmasterjalexander@gmail.com>
2020-12-14 13:36:02 +1100

    Time block rows HTML. Var's listed JS

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var dayPlanner9 = $("#9");
+var dayPlanner10 = $("#10");
+var dayPlanner11 = $("#11");
+var dayPlanner12 = $("#12");
+var dayPlanner1 = $("#13");
+var dayPlanner2 = $("#14");
+var dayPlanner3 = $("#15");
+var dayPlanner4 = $("#16");
+var dayPlanner5 = $("#17");
+var dayPlanner6 = $("#18")
+var time = moment();
+
